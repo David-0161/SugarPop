b0c8193954afe40fdb94c6db7b75578cfa1c380e
David-0161 <david.ozoagu@principia.edu>
2024-12-06 23:05:03 -0600

    Finished changes

--- bucket.py
+++ bucket.py
@@ -2,7 +2,7 @@
 # Module Name: Sugar Pop Bucket Module
 # Project: Sugar Pop Program
 # Date: Nov 17, 2024
-# By: Brett W. Huffman
+# By: David Ozoagu
 # Description: The bucket implementation of the sugar pop game
 #############################################################
 
